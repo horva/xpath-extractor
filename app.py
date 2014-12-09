12796a74562456bc10d7bfdde8e3739af2613b4a
Marko Horvatic <m.horva@gmail.com>
2014-12-09 13:49:44 +0100

    Use redis and session id in url to enable group debugging and sharing

--- app.py
+++ app.py
@@ -1,21 +1,60 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
+from uuid import uuid4
+import redis
+import json
 
 from forms import XPathExtractorForm
 
 
+def _get_redis_connection():
+    return redis.Redis()
+
+
+def _get_new_session_id():
+    return uuid4().hex
+
+
+def _save_session(data, session_id=None):
+    if not session_id:
+        session_id = _get_new_session_id()
+    data = json.dumps(data)
+    redis_connection = _get_redis_connection()
+    redis_connection.set(session_id, data)
+    return session_id
+
+
+def _load_session(session_id):
+    redis_connection = _get_redis_connection()
+    return json.loads(redis_connection.get(session_id))
+
+
 @app.route('/', methods=['POST', 'GET'])
-def home():
+@app.route('/<string:session_id>/', methods=['POST', 'GET'])
+def home(session_id=None):
     message = ''
     result = None
     form = XPathExtractorForm(request.form)
-    if request.method == 'POST' and form.validate():
+
+    if request.method == 'GET' and session_id:
+        form_kw = _load_session(session_id)
+        form = XPathExtractorForm(**form_kw)
+        result, html, xpath, subxpath = form.resolve()
+
+    elif request.method == 'POST' and form.validate():
         try:
             result, html, xpath, subxpath = form.resolve()
-            form = XPathExtractorForm(html=html, xpath=xpath, subxpath=subxpath)
+            form_kw = {
+                'html': html,
+                'xpath': xpath,
+                'subxpath': subxpath
+            }
+            session_id = _save_session(form_kw, session_id)
+            return redirect(url_for('home', session_id=session_id))
         except Exception as ex:
             message = unicode(ex)
+
     return render_template('base.html', form=form, result=result, message=message)
 
 
